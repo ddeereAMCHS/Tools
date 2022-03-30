2d26f365092b080bd5fdd4b3a2e34eaae5dd8823
Dawson Deere <ddeere@csisd.org>
2022-03-30 12:27:40 -0500

    added checks and comments

--- change-file.sh
+++ change-file.sh
@@ -8,11 +8,17 @@
 #   Do not include the path to this directory as that should be set in the BASE variable
 # <filename> is the filepath of the file in the repository that you want to copy to your students' repositories
 # <repo> is the prefix GitHub Classroom gives each student repository for this assignment
-# <period> is the class period you want to add a file for
+# <period> is the class period for which you want to change a file
+
+
+# directory on local machine that has GitHub repositories
+BASE=""
+# GitHub organization used for GitHub Classroom
+ORG=""
 
 # verify arguments
 if [ $# != 5 ]; then
-  echo "USAGE: change-file.sh <op> <dir> <filename> <repo> <period>"
+  echo "USAGE: change-file.sh <op> <dir> <filename> <repo> <period>" >&2
   exit 1
 fi
 
@@ -29,16 +35,35 @@ if [ ${OP} = "add" ]; then
 elif [ ${OP} = "rm" ]; then
   MSG="removed ${FILE}"
 else
-  echo "ERROR: not a valid operation"
+  echo "ERROR: not a valid operation" >&2
   exit 1
 fi
 
-# directory on local machine that has GitHub repositories
-BASE=""
-# GitHub organization used for GitHub Classroom
-ORG=""
+# verify DIR exists on local machine
+if [ ! -e ${BASE}/${DIR} ]; then
+  echo "ERROR: ${DIR} does not exist" >&2
+  exit 1
+fi
+if [ ! -d ${BASE}/${DIR} ]; then
+  echo "ERROR: ${DIR} is not a valid directory" >&2
+  exit 1
+fi
+
+# verify PERIOD is numeric
+re='^[0-9]$'
+if ! [[ $PERIOD  =~ $re ]]; then
+  echo "ERROR: period must be a number between 0 and 9 (inclusive)" >&2
+  exit 1
+fi
+
+# verify FILE exists on local machine
+if [ ! -e ${BASE}/${DIR}/${FILE} ]; then
+  echo "ERROR: ${DIR}/${FILE} does not exist" >&2
+  exit 1
+fi
 
 # list of github usernames per class period
+# enclose each username in quotes and separate with spaces
 usernames1=()
 usernames2=()
 usernames3=()
@@ -73,7 +98,7 @@ case "$PERIOD" in
     gitnames=${usernames7[*]}
     ;;
   *)
-    echo "ERROR: not a valid class period"
+    echo "ERROR: not a valid class period" >&2
     exit 1
     ;; 
 esac
@@ -81,25 +106,49 @@ esac
 # for debugging if you need to work on just one student repo
 #gitnames=""
 
-# add/modify file
+# make sure local repo is up to date
+cd ${BASE}/${DIR}
+git pull > /dev/null 2>&1
+
+# change file
 cd ${BASE}
 for name in $gitnames; do
   echo "Working on ${name}"
+  # clone repo
   git clone https://github.com/${ORG}/${REPO}-${name}.git > /dev/null 2>&1
   if [ $? -ne 0 ]; then
     echo "  WARNING: could not clone repo ${REPO}-${name}"
     continue
   fi
   echo "  cloned repo ${REPO}-${name}"
+  # copy file
   cd ${BASE}/${REPO}-${name}
   if [ ${OP} = "add" ]; then
-    cp ${BASE}/${DIR}/${FILE} ./${FILE}
+    tmp=${FILE}
+    # create directories if needed
+    while [[ "${tmp}" == *\/* ]]; do
+      dirtomake=$(echo $tmp | cut -d'/' -f 1)
+      tmp=${tmp:${#dirtomake}+1}
+      if [ ! -d ${dirtomake} ]; then
+        mkdir ${dirtomake}
+      fi
+      cd ${dirtomake}
+    done
+    cp ${BASE}/${DIR}/${FILE} .
   fi
+  # add, commit, and push changes
+  cd ${BASE}/${REPO}-${name}
   git ${OP} ${FILE} > /dev/null 2>&1
   git commit -m "${MSG}" > /dev/null 2>&1
-  git push > /dev/null 2>&1
-  echo "  ${MSG} for ${name}"
+  pushOutput=$(git push 2>&1)
+  # check if file was changed or not
+  if [[ ${pushOutput} = "Everything up-to-date" ]]; then
+    echo "  no changes for ${name}"
+  else
+    echo "  ${MSG} for ${name}"
+  fi
   cd ${BASE} > /dev/null
+  # remove cloned repo
   rm -rf ${BASE}/${REPO}-${name}
   echo "  removed repo ${REPO}-${name}"
 done
